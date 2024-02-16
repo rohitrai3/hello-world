7abba442bd0e32d925c24fdd145d97b0ae2a07a8
Rohit Rai <rohitraijob@gmail.com>
2024-02-16 13:05:33 +0530

    fix: move text to center

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="w-dvw h-dvh bg-black text-white flex items-center text-center">
+    <div className="w-dvw h-dvh bg-black text-white flex justify-center items-center text-center">
       <h1 className="text-9xl font-black tracking-widest">Hello World</h1>
     </div>
   );
