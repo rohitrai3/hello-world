39113190f6e455ae6d3357c5cad53a2147e9564a
Rohit Rai <rohitraijob@gmail.com>
2024-02-16 12:56:11 +0530

    feat: privacy policy page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function Privacy() {
+  return (
+    <div className="w-dvw h-dvh bg-black text-white flex flex-col justify-center text-center space-y-2">
+      <h1 className=" text-xl font-black tracking-widest">Privacy Policy</h1>
+      <p>We don&apos;t collect or share any data.</p>
+    </div>
+  );
+}
