dc5311a7515e66b859ee2b8b2031bde8fc7e8f27
Rohit Rai <rohitraijob@gmail.com>
2024-02-16 12:55:56 +0530

    feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="w-dvw h-dvh bg-black text-white flex items-center text-center">
+      <h1 className="text-9xl font-black tracking-widest">Hello World</h1>
+    </div>
+  );
 }
